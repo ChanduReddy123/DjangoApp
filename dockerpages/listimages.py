452b746eda43b9d74c2c4c25ede7c4ad266b8149
chandureddy123 <reddychandu1995@gmail.com>
2019-10-07 14:36:00 +0530

    list images bug fix

--- dockerpages/listimages.py
+++ dockerpages/listimages.py
@@ -1,12 +1,26 @@
 def ListImages(remote_Machine):
     result = remote_Machine.images()
+    print(result)
     data = []
     for list in result:
         image = []
-        memory =   "{0:.2f}Mb".format(list['Size']/1024/1024)
-        name = list['RepoTags'][0]
-        image.append(name)
-        image.append(memory)
-        data.append(image)
+        memory =   "{0:.2f}Mb".format(list['Size']/1000/1000)
+        if(list['RepoTags']):        
+            for tags in list['RepoTags']:
+                name = tags
+                image.append(name)
+                image.append(memory)
+                data.append(image)
+        elif(list['RepoDigests']):
+            name = list['RepoDigests'][0].split('@')[0]
+            image.append(name)
+            image.append(memory)
+            data.append(image)
+        else:
+            name='none'
+            image.append(name)
+            image.append(memory)
+            data.append(image)
+         
     #ListContainers()
     return data
